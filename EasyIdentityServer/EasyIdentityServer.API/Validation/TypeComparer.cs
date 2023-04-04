0f2599ade3d27e5f77ec1317b2581bd2946514bc
Roman <belousov.r@helix.ru>
2023-04-04 22:46:21 +0300

    Add references

---
+++ EasyIdentityServer/EasyIdentityServer.API/Validation/TypeComparer.cs
@@ -0,0 +1,16 @@
+namespace EasyIdentityServer.API.Validation;
+
+public class TypeComparer : IEqualityComparer<Type>
+{
+    public bool Equals(Type? x, Type? y)
+    {
+        if (x == null || y == null)
+            return false;
+        return x.FullName == y.FullName;
+    }
+
+    public int GetHashCode(Type obj)
+    {
+        return obj.GetHashCode();
+    }
+}
---
+++ EasyIdentityServer/EasyIdentityServer.API/Validation/ValidationOptionsProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using EasyIdentityServer.DTO.Exception;
+using EasyTradeLibs.Abstractions;
+using EasyTradeLibs.Validation;
+
+namespace EasyIdentityServer.API.Validation;
+
+public class ValidationOptionsProvider : IValidationOptionsProvider
+{
+    private Dictionary<Type, ValidationOptions> _options;
+
+    public ValidationOptionsProvider()
+    {
+        _options = new Dictionary<Type, ValidationOptions>(new TypeComparer())
+        {
+            {
+                typeof(InvalidCredentialsException), 
+                new ValidationOptions()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                }
+            },
+            {
+                typeof(RegisterExistUserException), 
+                new ValidationOptions()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                }
+            },
+            {
+                typeof(UserNotExistException), 
+                new ValidationOptions()
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest
+                }
+            },
+        };
+    }
+
+    public Dictionary<Type, ValidationOptions> Get() => _options;
+}
